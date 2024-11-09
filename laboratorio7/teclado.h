6a35154d8e458582c56b90b6e9773b9f0beb3f0c
jedg73 <57274038+jedg73@users.noreply.github.com>
2024-11-08 20:14:08 -0500

    Update teclado.h

--- laboratorio7/teclado.h
+++ laboratorio7/teclado.h
@@ -1,67 +1,33 @@
 
-#include<xc.h>
-#include "teclado.h"
+#define TECLADO_H
+#define	TECLADO_H
 
-#define _XTAL_FREQ 4000000UL
+#define PB7     7
+#define PB6     6
+#define PB5     5
+#define PB4     4
+#define PB3     3
+#define PB2     2
+#define PB1     1
+#define PB0     0
 
+/* FILAS - SALIDAS */
+#define ROW1    PB0
+#define ROW2    PB1
+#define ROW3    PB2
+#define ROW4    PB3
 
+/* COLUMNAS - ENTRADAS */
+#define COL1    PB4
+#define COL2    PB5
+#define COL3    PB6
+#define COL4    PB7
 
-void teclado_init()      
-{
-    PTECLADO &= ~(1<<PB0) & ~(1<<PB1) & ~(1<<PB2) & ~(1<<PB3);
-    PTECLADO |= (1<<PB5) | (1<<PB6) | (1<<PB7);
-                                          
-}
+#define PTECLADO TRISB
+#define FILAS LATB
+#define COLUMNAS PORTB
 
-char teclado_getc()
-{
-    char letras[4][4] = {{'1','2','3','A'},
-                         {'4','5','6','B'},
-                         {'7','8','9','C'},
-                         {'*','0','#','D'}};
-    
-    int i=0;
-    char valor=0;
-    
-    for(i=0;i<4;i++)
-    {
-        if(i==0)
-        {
-          FILAS = (1<<COL4) | (1<<COL3) | (1<<COL2) | (1<<COL1) | (1<<ROW4) | (1<<ROW3) | (1<<ROW2) | (0<<ROW1);
-          __delay_us(100);
-          while(!(COLUMNAS & (1<<COL1))){ valor = letras[0][0];}   // 1
-          while(!(COLUMNAS & (1<<COL2))){ valor = letras[0][1];}   // 2
-          while(!(COLUMNAS & (1<<COL3))){ valor = letras[0][2];}   // 3
-          while(!(COLUMNAS & (1<<COL4))){ valor = letras[0][3];}   // A
-        }
-        if(i==1)
-        {
-          FILAS = (1<<COL4) | (1<<COL3) | (1<<COL2) | (1<<COL1) | (1<<ROW4) | (1<<ROW3) | (0<<ROW2) | (1<<ROW1);
-          __delay_us(100);
-          while(!(COLUMNAS & (1<<COL1))){ valor = letras[1][0];}   // 4
-          while(!(COLUMNAS & (1<<COL2))){ valor = letras[1][1];}   // 5
-          while(!(COLUMNAS & (1<<COL3))){ valor = letras[1][2];}   // 6
-          while(!(COLUMNAS & (1<<COL4))){ valor = letras[1][3];}   // B
-        }
-        if(i==2)
-        {
-          FILAS = (1<<COL4) | (1<<COL3) | (1<<COL2) | (1<<COL1) | (1<<ROW4) | (0<<ROW3) | (1<<ROW2) | (1<<ROW1);
-          __delay_us(100);
-          while(!(COLUMNAS & (1<<COL1))){ valor = letras[2][0];}   // 7
-          while(!(COLUMNAS & (1<<COL2))){ valor = letras[2][1];}   // 8
-          while(!(COLUMNAS & (1<<COL3))){ valor = letras[2][2];}   // 9
-          while(!(COLUMNAS & (1<<COL4))){ valor = letras[2][3];}   // C
-        }
-        if(i==3)
-        {
-          FILAS = (1<<COL4) | (1<<COL3) | (1<<COL2) | (1<<COL1) | (0<<ROW4) | (1<<ROW3) | (1<<ROW2) | (1<<ROW1);
-          __delay_us(100);
-          while(!(COLUMNAS & (1<<COL1))){ valor = letras[3][0];}   // *
-          while(!(COLUMNAS & (1<<COL2))){ valor = letras[3][1];}   // 0
-          while(!(COLUMNAS & (1<<COL3))){ valor = letras[3][2];}   // #
-          while(!(COLUMNAS & (1<<COL4))){ valor = letras[3][3];}   // D
-        }
-    }
-    
-    return valor;
-}
+void teclado_init();
+char teclado_getc();
+
+#endif	/* TECLADO_H */
